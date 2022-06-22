df5214cf843bdba886c29edc353038cc8d9e0845
KevinPena2022 <pen21006@byui.edu>
2022-06-22 11:57:20 -0700

    Collission for itself added

--- cycle/game/scripting/handle_collissions2.py
+++ cycle/game/scripting/handle_collissions2.py
@@ -25,23 +25,23 @@ class HandleCollisionsAction2(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        # if not self._is_game_over:
-        #     self._handle_segment_collision(cast)
-        #     self._handle_game_over(cast)
+        if not self._is_game_over:
+            self._handle_segment_collision(cast)
+            self._handle_game_over(cast)
     
-    # def _handle_segment_collision(self, cast):
-    #     """Sets the game over flag if the snake collides with one of its segments.
+    def _handle_segment_collision(self, cast):
+        """Sets the game over flag if the snake collides with one of its segments.
         
-    #     Args:
-    #         cast (Cast): The cast of Actors in the game.
-    #     """
-    #     snake = cast.get_first_actor("snakes2")
-    #     head = snake.get_segments()[0]
-    #     segments = snake.get_segments()[1:]
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        snake = cast.get_first_actor("snakes2")
+        head = snake.get_segments()[0]
+        segments = snake.get_segments()[1:]
         
-    #     for segment in segments:
-    #         if head.get_position().equals(segment.get_position()):
-    #             self._is_game_over = True
+        for segment in segments:
+            if head.get_position().equals(segment.get_position()):
+                self._is_game_over = True
         
     def _handle_game_over(self, cast):
         """Shows the 'game over' message and turns the snake white if the game is over.
